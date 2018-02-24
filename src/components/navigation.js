5eb120351ab99054a30f673902c4577666ae8346
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-23 21:07:38 -0800

    Basic styling

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faExchangeAlt, faChartLine, faCogs, faShare, faSignOutAlt, faInfo,  } from '@fortawesome/fontawesome-free-solid'
+import { faExchangeAlt, faChartLine, faCogs, faShare, faSignOutAlt, faInfo } from '@fortawesome/fontawesome-free-solid'
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-    <nav>
-        <div><Link to='/dashboard'>Dashboard</Link></div>
-        <div><Link to='/exchange'>Exchange</Link></div>
-        <div><Link to='/send'>Send</Link></div>
-        <div><Link to='/account'>Account</Link></div>
-        <div>
-          <Link to='/info'>Info</Link>
-          <Link to='/exit'>Exit</Link>
-        </div>
-    </nav>
+  <header className="b-sidebar">
+    <div className="nav-icon"><Link to='/dashboard'><FontAwesomeIcon icon={faChartLine} /></Link></div>
+    <div className="nav-icon"><Link to='/exchange'><FontAwesomeIcon icon={faExchangeAlt} /></Link></div>
+    <div className="nav-icon"><Link to='/send'><FontAwesomeIcon icon={faShare} /></Link></div>
+    <div className="nav-icon"><Link to='/account'><FontAwesomeIcon icon={faCogs} /></Link></div>
+    <div className="nav-icon">
+      <Link to='/info'><FontAwesomeIcon icon={faInfo} /></Link>
+      <Link to='/exit'><FontAwesomeIcon icon={faSignOutAlt} /></Link>
+    </div>
   </header>
 )
 
