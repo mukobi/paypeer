4b3f536faa2fbb46b661a8819c11a7d9b5425099
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-23 23:06:17 -0800

    Make link clickboxes fill parents in nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,13 +6,13 @@ import '../images/paypeer_logo.png';
 // between routes.
 const Header = () => (
   <header className="b-sidebar">
-    <div className="logo"><Link to='/dashboard'><img src={'./images/paypeer_logo.png'} /></Link></div>
+    <div className="logo"><a href="https://ethanls7.wixsite.com/paypeer"><img src={'./images/paypeer_logo.png'} /></a></div>
     <div className="nav-icon"><Link to='/dashboard'><i className="fas fa-chart-line" /></Link></div>
     <div className="nav-icon"><Link to='/exchange'><i className="fas fa-exchange-alt" /></Link></div>
     <div className="nav-icon"><Link to='/send'><i className="fas fa-share" /></Link></div>
     <div className="nav-icon"><Link to='/account'><i className="fas fa-cogs" /></Link></div>
     <div className="nav-icon"><Link to='/info'><i className="fas fa-info" /></Link></div>
-    <div className="nav-icon"><Link to='/exit'><i className="fas fa-sign-out-alt" /></Link></div>
+    <div className="nav-icon"><a href="https://ethanls7.wixsite.com/paypeer"><i className="fas fa-sign-out-alt" /></a></div>
   </header>
 )
 
