ad66f8889a14696b3701b6f74a8beb063ce42cc5
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-24 13:52:31 -0800

    Style exchange widget

--- src/components/exchange-widget.js
+++ src/components/exchange-widget.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import '../style/exchange-widget.css';
 
 const ExchangeWidget = () => (
-    <div>
-        <div>
-		<input id="you-have" type="number" name="you have" placeholder="you have" />
-            <select className="currency-type" id="primary" name="exchanged_from">
+    <div className="b-exchange-widget">
+        <div className="input-area">
+			<input className="currency-input" id="you-have" type="number" name="you have" placeholder="You have" />
+			<select className="currency-type" id="primary" name="exchanged_from">
 						<option>ETH</option>
 						<option>ZEC</option>
 						<option>DASH</option>
 						<option>XRP</option>
 						<option>XMR</option>
 						<option>LTC</option>
-					</select>
+			</select>
         </div>
         <i className="fas fa-exchange-alt" />
-        <div>
-            <input id="you-get" readOnly="true" type="number" name="you get" placeholder="you Get" />
+        <div className="input-area">
+            <input className="currency-input" id="you-get" readOnly="true" type="number" name="you get" placeholder="You get" />
             <select className="currency-type" id="secondary" name="exchanged_to">
 						<option>LTC</option>
 						<option>ETH</option>
