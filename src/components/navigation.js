ab258d5872a0fe85fbf6f45272ee8752acb47cad
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-25 18:52:41 -0800

    Switch from font awesome to png icons for nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../images/paypeer_logo.png';
+
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Navigation = () => (
   <header className="b-sidebar">
     <div className="logo">
-      <a href="https://ethanls7.wixsite.com/paypeer"><img src={'./images/paypeer_logo.png'} /></a>
+      <a href="https://ethanls7.wixsite.com/paypeer"><img src={require('../images/paypeer_logo.png')} /></a>
       </div>
     <div className="nav-icon">
-      <NavLink to='/dashboard' activeClassName="active"><i className="fas fa-chart-line" /></NavLink>
+      <NavLink to='/dashboard' activeClassName="active"><img src={require('../images/icons/dashboard.png')} /></NavLink>
     </div>
     <div className="nav-icon">
-      <NavLink to='/exchange' activeClassName="active"><i className="fas fa-exchange-alt" /></NavLink>
+      <NavLink to='/exchange' activeClassName="active"><img src={require('../images/icons/exchange.png')} /></NavLink>
     </div>
     <div className="nav-icon">
-      <NavLink to='/send' activeClassName="active"><i className="fas fa-share" /></NavLink>
+      <NavLink to='/send' activeClassName="active"><img src={require('../images/icons/send.png')} /></NavLink>
     </div>
     <div className="nav-icon">
-      <NavLink to='/account' activeClassName="active"><i className="fas fa-cogs" /></NavLink>
+      <NavLink to='/account' activeClassName="active"><img src={require('../images/icons/account.png')} /></NavLink>
     </div>
     <div className="nav-icon">
-      <NavLink to='/info' activeClassName="active"><i className="fas fa-info" /></NavLink>
+      <NavLink to='/info' activeClassName="active"><img src={require('../images/icons/help.png')} /></NavLink>
     </div>
     <div className="nav-icon">
-    <a href="https://ethanls7.wixsite.com/paypeer"><i className="fas fa-sign-out-alt" /></a></div>
+    <a href="https://ethanls7.wixsite.com/paypeer"><img src={require('../images/icons/logout.png')} /></a></div>
   </header>
 )
 
